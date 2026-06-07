6cb6e4761349ce901ccad969884310692e4e1db0
erotourtes <erotourtes@gmail.com>
2026-06-07 23:46:36 +0200

    feat: qrcode

---
+++ scripts/gen-qrcode.fish
@@ -0,0 +1,11 @@
+#!/usr/bin/env fish
+
+set -l current_file_path (status current-filename)
+set -l current_file_dir (dirname $current_file_path)
+set -l root_dir (realpath "$current_file_dir/..")
+
+set -l url "https://github.com/erotourtes/mirage"
+
+qrencode -o "$root_dir/thesis/pictures/qr.png" -s 10 -l H $url
+oxipng -o max --strip safe --out "$root_dir/thesis/pictures/output.png" "$root_dir/thesis/pictures/qr.png"
+mv "$root_dir/thesis/pictures/output.png" "$root_dir/thesis/pictures/qr.png"
---
+++ thesis/diagrams/diagram2svg.typ
@@ -0,0 +1,15 @@
+#import "diagrams.typ": diagrams
+#import "protocol_diagrams.typ": protocol-diagrams
+
+#set page(width: auto, height: auto, margin: 2pt, fill: none)
+
+// typst compile --format svg ./thesis/diagrams/diagram2svg.typ dig.svg
+
+// #diagrams.ot-extended
+// #diagrams.three-way-merge
+// #diagrams.subset-lattice
+// #diagrams.attribute-markers
+// #diagrams.delete-set-merge
+// #diagrams.lamport-clocks
+// #diagrams.local-insert-state
+// #protocol-diagrams.update-layout
